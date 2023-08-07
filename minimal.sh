569d337475bbdaf2471d6554da311e800e94cd8a
0kilobytes <96943686+0kilobytes@users.noreply.github.com>
2023-08-07 09:26:37 +0000

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -1,1 +1,9 @@
 printf '\n`minimal install goes here\n'
+
+ping -c 1 archlinux.org > /dev/null
+if [[ $? -ne 0 ]] ; then
+    echo "Please check the internet connection."
+    exit 1
+else
+    echo "Internet OK."
+fi
